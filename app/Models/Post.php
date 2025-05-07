f9a6bb3954c9f3c5fd7ce6e4a927c8dd4a4e11d2
youssef hussein <20634930+youssefhussein@users.noreply.github.com>
2025-05-07 19:32:50 +0300

    create post model

---
+++ app/Models/Post.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Post extends Model
+{
+    /** @use HasFactory<\Database\Factories\PostFactory> */
+    use HasFactory;
+    protected $fillable=[
+        "recipename",
+        "text",
+        "type"
+    ];
+}
