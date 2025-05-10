59ca31d2e0414805878fc0635e63205ad3ebffad
youssef hussein <20634930+youssefhussein@users.noreply.github.com>
2025-05-10 23:01:56 +0300

    loop worksss

--- resources/js/pages/dashboard.tsx
+++ resources/js/pages/dashboard.tsx
@@ -1,7 +1,6 @@
 import CreatePostForm from '@/components/posts/create-post';
-import MyPost from '@/components/posts/postcard';
 import AppLayout from '@/layouts/app-layout';
-import { type BreadcrumbItem } from '@/types';
+import { Post, type BreadcrumbItem } from '@/types';
 import { Head } from '@inertiajs/react';
 
 const breadcrumbs: BreadcrumbItem[] = [
@@ -10,18 +9,31 @@ const breadcrumbs: BreadcrumbItem[] = [
         href: '/dashboard',
     },
 ];
+//i want to get props from the server , posts exist inside data object
+export default function Dashboard( 
+    props: { posts: { data: Post[] } }
+) {
 
-export default function Dashboard() {
-    // const { auth } = usePage<SharedData>().props;
-
+    
     return (
         <AppLayout breadcrumbs={breadcrumbs}>
             <Head title="Dashboard" />
             <div className="flex h-full w-full flex-1 flex-row justify-center gap-4 rounded-xl p-4">
-                <div>
-                    <CreatePostForm />
-                    <MyPost></MyPost>
-                </div>
+                <CreatePostForm />
+                <div className="flex flex-col gap-4">
+                    {props.posts.data.map((post) => (
+                        <div key={post.id} className=" ">
+                            <div className="flex flex-row gap-2">
+                                <div className="w-10 h-10 rounded-full bg-gray-200"></div>
+                                <div className="flex flex-col gap-1">
+                                    <div className="text-sm font-medium text-gray-900">{post.description}</div>
+                                    <div className="text-xs text-gray-500">{post.name}</div>
+                                </div>
+                            </div>
+                            <div className="text-sm text-gray-500">{post.ingredients}</div>
+                        </div>
+                    ))}
+                    </div>
             </div>
         </AppLayout>
     );
